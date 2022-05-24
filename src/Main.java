0eff069ca8600b0efea51f42f2d486d9823c5255
kundusaikat <kundusaikat32@gmail.com>
2022-05-24 08:48:54 +0530

    Hello World

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is My First Java Application");
+        System.out.println("==============================");
+        System.out.println("My name is : Saikat Kundu");
+        System.out.println("My Address is: Bankura, West Bengal");
+    }
+
+    public static int other(String[] args) {
+        System.out.println("Hello other");
+        return 0;
+
+    }
+}
